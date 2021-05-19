de03fca2f382ed6a10bf99819bc633305e0e2fd7
FoLZer <nikolaenko.gleb.044@gmail.com>
2021-05-19 21:04:48 +0300

    Clean up Main class

--- src/main/java/de/maxhenkel/voicechat/Voicechat.java
+++ src/main/java/de/maxhenkel/voicechat/Voicechat.java
@@ -1,32 +1,20 @@
 package de.maxhenkel.voicechat;
 
-import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.wrappers.MinecraftKey;
 import com.google.common.io.ByteArrayDataInput;
 import com.google.common.io.ByteStreams;
-import de.maxhenkel.voicechat.api.FriendlyByteBuf;
 import de.maxhenkel.voicechat.api.ResourceLocation;
 import de.maxhenkel.voicechat.command.VoicechatCommands;
 import de.maxhenkel.voicechat.command.VoicechatCommandsAutocompleter;
 import de.maxhenkel.voicechat.config.ConfigBuilder;
 import de.maxhenkel.voicechat.config.ServerConfig;
+import de.maxhenkel.voicechat.events.PlayerJoinListener;
 import de.maxhenkel.voicechat.voice.server.ServerVoiceEvents;
-import io.netty.buffer.Unpooled;
 import net.kyori.adventure.text.Component;
-import net.minecraft.server.v1_16_R3.PacketDataSerializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.plugin.messaging.PluginMessageListener;
-import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
 import java.io.InputStream;
@@ -40,14 +28,28 @@ public class Voicechat extends JavaPlugin {
     public static ServerVoiceEvents SERVER;
     @Nullable
     public static ServerConfig SERVER_CONFIG;
+    public static final ResourceLocation FABRIC_INIT = new ResourceLocation(MODID, "init");
+    public static final ResourceLocation FORGE_DEFAULT = new ResourceLocation(MODID, "default");
 
-    public static final ResourceLocation INIT = new ResourceLocation(Voicechat.MODID, "init");
     public static int COMPATIBILITY_VERSION = -1;
-
     public static final Pattern GROUP_REGEX = Pattern.compile("^[a-zA-Z0-9-_]{1,16}$");
+
+    public static ProtocolManager PROTOCOL_MANAGER = ProtocolLibrary.getProtocolManager();
 
     @Override
     public void onEnable() {
+        loadConfig();
+        registerChannels();
+
+        getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
+
+        SERVER = new ServerVoiceEvents(this);
+
+        getCommand("voicechat").setExecutor(new VoicechatCommands(this));
+        getCommand("voicechat").setTabCompleter(new VoicechatCommandsAutocompleter());
+    }
+
+    private void loadConfig() {
         try {
             LOGGER.info(getClass().getClassLoader());
             InputStream in = getClass().getClassLoader().getResourceAsStream("compatibility.properties");
@@ -59,53 +61,29 @@ public class Voicechat extends JavaPlugin {
             LOGGER.error("Failed to read compatibility version");
         }
         ConfigBuilder.create(getDataFolder().toPath().resolve("config").resolve(MODID).resolve("voicechat-server.properties"), builder -> SERVER_CONFIG = new ServerConfig(builder));
+    }
+    private void registerChannels() {
+        FABRIC_INIT.registerIncomingChannel(this, (channel, player, message) -> {
+            ByteArrayDataInput in = ByteStreams.newDataInput(message);
+            int clientCompatibilityVersion = in.readInt();
+            LOGGER.info("Got INIT answer : {}", clientCompatibilityVersion);
 
-        INIT.registerIncomingChannel(this, new PluginMessageListener() {
-            @Override
-            public void onPluginMessageReceived(@NotNull String channel, @NotNull Player player, @NotNull byte[] message) {
-                ByteArrayDataInput in = ByteStreams.newDataInput(message);
-                int clientCompatibilityVersion = in.readInt();
-                LOGGER.info("Got INIT answer : {}", clientCompatibilityVersion);
-
-                if (clientCompatibilityVersion != Voicechat.COMPATIBILITY_VERSION) {
-                    Voicechat.LOGGER.warn("Client {} has incompatible voice chat version (server={}, client={})", player.getAddress().getAddress().getHostAddress(), Voicechat.COMPATIBILITY_VERSION, clientCompatibilityVersion);
-                    player.kick(Component.translatable("message.voicechat.incompatible_version"));
-                }
+            if (clientCompatibilityVersion != Voicechat.COMPATIBILITY_VERSION) {
+                Voicechat.LOGGER.warn("Client {} has incompatible voice chat version (server={}, client={})", player.getAddress().getAddress().getHostAddress(), Voicechat.COMPATIBILITY_VERSION, clientCompatibilityVersion);
+                player.kick(Component.translatable("message.voicechat.incompatible_version"));
             }
         });
-        INIT.registerOutgoingChannel(this);
-        class QUERY_START implements Listener {
-            final Voicechat main;
-            QUERY_START(Voicechat main) {
-                this.main = main;
-            }
-            @EventHandler
-            public void onLogin(PlayerJoinEvent e) {
-                Bukkit.getScheduler().scheduleSyncDelayedTask(main, () -> {
-                    ProtocolManager protocolManager = ProtocolLibrary.getProtocolManager();
-                    PacketContainer packet = protocolManager.createPacket(PacketType.Play.Server.CUSTOM_PAYLOAD);
-                    try {
-                        FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.buffer());
-                        buf.writeInt(COMPATIBILITY_VERSION);
-                        packet.getMinecraftKeys().write(0, new MinecraftKey("voicechat","init"));
-                        PacketDataSerializer packetDataSerializer = new PacketDataSerializer(Unpooled.buffer());
-                        packetDataSerializer.writeInt(buf.readableBytes());
-                        packetDataSerializer.writeBytes(buf);
-                        packet.getModifier().withType(PacketDataSerializer.class).write(0, packetDataSerializer);
-                        protocolManager.sendServerPacket(e.getPlayer(), packet);
-                    } catch(Exception ex) {
-                        ex.printStackTrace();
-                    }
-                    LOGGER.info("Sent INIT packet");
-                }, 20);
-            }
-        }
-        getServer().getPluginManager().registerEvents(new QUERY_START(this), this);
+        FABRIC_INIT.registerOutgoingChannel(this);
+        FORGE_DEFAULT.registerIncomingChannel(this, (channel, player, message) -> {
+            ByteArrayDataInput in = ByteStreams.newDataInput(message);
+            int clientCompatibilityVersion = in.readInt();
+            LOGGER.info("Got INIT answer : {}", clientCompatibilityVersion);
 
-        SERVER = new ServerVoiceEvents(this);
-
-        //CommandRegistrationCallback.EVENT.register(VoicechatCommands::register);
-        getCommand("voicechat").setExecutor(new VoicechatCommands(this));
-        getCommand("voicechat").setTabCompleter(new VoicechatCommandsAutocompleter());
+            if (clientCompatibilityVersion != Voicechat.COMPATIBILITY_VERSION) {
+                Voicechat.LOGGER.warn("Client {} has incompatible voice chat version (server={}, client={})", player.getAddress().getAddress().getHostAddress(), Voicechat.COMPATIBILITY_VERSION, clientCompatibilityVersion);
+                player.kick(Component.translatable("message.voicechat.incompatible_version"));
+            }
+        });
+        FORGE_DEFAULT.registerOutgoingChannel(this);
     }
 }
---
+++ src/main/java/de/maxhenkel/voicechat/events/PlayerJoinListener.java
@@ -0,0 +1,40 @@
+package de.maxhenkel.voicechat.events;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.wrappers.MinecraftKey;
+import de.maxhenkel.voicechat.Voicechat;
+import de.maxhenkel.voicechat.api.FriendlyByteBuf;
+import io.netty.buffer.Unpooled;
+import net.minecraft.server.v1_16_R3.PacketDataSerializer;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerJoinListener implements Listener {
+    final Voicechat main;
+    public PlayerJoinListener(Voicechat main) {
+        this.main = main;
+    }
+
+    @EventHandler
+    public void onLogin(PlayerJoinEvent e) {
+        Bukkit.getScheduler().scheduleSyncDelayedTask(main, () -> {
+            PacketContainer packet = Voicechat.PROTOCOL_MANAGER.createPacket(PacketType.Play.Server.CUSTOM_PAYLOAD);
+            try {
+                FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.buffer());
+                buf.writeInt(Voicechat.COMPATIBILITY_VERSION);
+                packet.getMinecraftKeys().write(0, new MinecraftKey("voicechat","init"));
+                PacketDataSerializer packetDataSerializer = new PacketDataSerializer(Unpooled.buffer());
+                packetDataSerializer.writeInt(buf.readableBytes());
+                packetDataSerializer.writeBytes(buf);
+                packet.getModifier().withType(PacketDataSerializer.class).write(0, packetDataSerializer);
+                Voicechat.PROTOCOL_MANAGER.sendServerPacket(e.getPlayer(), packet);
+            } catch(Exception ex) {
+                ex.printStackTrace();
+            }
+            Voicechat.LOGGER.info("Sent INIT packet");
+        }, 20);
+    }
+}
